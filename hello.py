6fe83ccfffa2d684103edf8b48cc4bc0ea2c6325
Sergiu-Hudrea <sergiu.hudrea@gmail.com>
2022-09-10 16:33:28 +0300

    test hello

---
+++ hello.py
@@ -0,0 +1,12 @@
+def main():
+    print("salut bobi")
+    print("salut sergiu")
+    print(99 * 100)
+    print("salut bobi")
+    print("salut sergiu")
+    print(99 * 100)
+
+
+
+if __name__ == "__main__":
+    main()
